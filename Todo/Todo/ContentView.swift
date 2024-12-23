1b43e1b5fb775b54800f27bed6b2126b211d4aed
이효주 <julie08051@naver.com>
2024-12-23 14:17:45 +0900

    Feat: AddTodoView, ContentView에서 sheet로 AddTodoView를 불러옴

---
+++ Todo/Todo/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Todo App
+//
+//  Created by 이효주 on 12/18/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var showingAddTodoView: Bool = false
+    
+    // MARK: - BODY
+    var body: some View {
+        NavigationView {
+            List(0 ..< 5) { item in Text("Hello, world!")
+            } //: LIST
+            .navigationBarTitle("Todo", displayMode: .inline)
+            .navigationBarItems(trailing:
+                Button(action: {
+                self.showingAddTodoView.toggle()
+                }) {
+                    Image(systemName: "plus")
+                } //: ADD BUTTON
+                .sheet(isPresented: $showingAddTodoView) {
+                    AddTodoView()
+                }
+            )
+        } //: NAVIGATION
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ Todo/Todo/TodoApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 이효주 on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
